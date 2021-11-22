495ca3932766145118bd95691129e55206d6967a
Gabs343 <gabriellopezp343@gmail.com>
2021-11-22 15:11:17 -0300

    bottom navigation

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+
+import { HomeScreen } from './Screen/HomeScreen';
+import { ApiTestScreen } from './Screen/ApiTestScreen';
+import { LoginScreen } from './Screen/LoginScreen'; 
+
+export default class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      ...DefaultTheme,
+      colors: {
+        myOwnColor: 'white',
+      },
+    }
+
+  }
+
+  render(){
+    let {theme} = this.state;
+    return (
+      <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Menu.Navigator
+          initialRouteName="Home"
+        >
+            <Menu.Screen 
+              name="Home" 
+              component={HomeScreen} 
+              options={{
+                tabBarIcon: ({color}) => (
+                  <MaterialIcons name="home" color={color} size={26}/>
+                ),
+              }}
+            />
+            <Menu.Screen 
+              name="Api" 
+              component={ApiTestScreen}
+              options={{
+                tabBarIcon: ({color}) => (
+                  <MaterialIcons name="api" color={color} size={26}/>
+                ),
+              }}
+            />
+            <Menu.Screen 
+              name="Login" 
+              component={LoginScreen}
+              options={{
+                tabBarIcon: ({color}) => (
+                  <MaterialIcons name="login" color={color} size={26}/>
+                ),
+              }}
+            />
+        </Menu.Navigator>
+      </NavigationContainer>
+      </PaperProvider>
+    );
+
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Menu = createBottomTabNavigator();
