c1e30c6a980b82898182894662be615594638be8
Gabs343 <gabriellopezp343@gmail.com>
2021-11-28 22:04:57 -0300

    quitando comentarios

--- routes/solarSystem.js
+++ routes/solarSystem.js
@@ -22,26 +22,6 @@ router.get("/:name", async (req, res) => {
     }
 });
 
-/*router.get("/planet/", async (req, res) =>{
-    try{
-        const planets = await SolarSystem.find({category: "Planet"});
-        res.header("Access-Control-Allow-Origin", "*");
-        res.json(planets);
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-});
-
-router.get("/planet/:name", async (req, res) =>{
-    try{
-        const planet = await SolarSystem.find({name: req.params.name, category: "planet"});
-        res.header("Access-Control-Allow-Origin", "*");
-        res.json(planet);
-    }catch(error){
-        res.status(500).json({message: error.message});
-    }
-});*/
-
 router.post("/", async(req, res) =>{
     const element = new SolarSystem({
         src: req.body.src,
