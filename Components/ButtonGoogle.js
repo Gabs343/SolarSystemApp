0b4585b5c3604f98fb44441dc37141c2da178f9e
Gabs343 <gabriellopezp343@gmail.com>
2021-12-04 15:33:12 -0300

    google log

--- Components/ButtonGoogle.js
+++ Components/ButtonGoogle.js
@@ -8,8 +8,8 @@ import * as Google from 'expo-auth-session/providers/google';
 export function ButtonGoogle({navigation}){
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        expoClientId: `779959306407-u1p7pndro01tqg78le401o7h610d2nkd.apps.googleusercontent.com`,
-        androidClientId: `779959306407-6fqup102d95lnno6o8lpkn060uecd59u.apps.googleusercontent.com`
+        expoClientId: `.apps.googleusercontent.com`,
+        androidClientId: `.apps.googleusercontent.com`
     });
 
     React.useEffect( () => {
