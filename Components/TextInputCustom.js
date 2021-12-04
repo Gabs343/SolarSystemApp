805aa53f3904eadffe5b358fc902e9d6baf48e36
Gabs343 <gabriellopezp343@gmail.com>
2021-12-04 15:31:31 -0300

    set props

--- Components/TextInputCustom.js
+++ Components/TextInputCustom.js
@@ -16,6 +16,7 @@ export class TextInputCustom extends Component{
                     placeholder={this.props.placeholder}
                     onChangeText={this.props.onChangeText}
                     theme={{colors: {text: "white"}}}
+                    secureTextEntry={this.props.secureTextEntry}
                 />
             
         );
